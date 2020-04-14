8321613612a281486e29722efed2367eea276358
panchorc1 <panchorc1@gmai.com>
2020-04-13 23:00:00 -0600

    Se agrega Api de Github al archivo Javascript

---
+++ js/Logica.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $('#Buscar').on('keyup', function (e) {
+        let nombre = e.target.value;
+        // Haciendo la peticion de la Api de Github
+        $.ajax({
+            // Variable url que que contiene la direccion de la Api Github con el nombre que le ingresemos    
+            url: 'https://api.github.com/users/' + nombre,
+            data: {
+                client_id: 'b9315bcd5a07fcd759d8',
+                client_secret: 'a2b698bf7e7c02f898197cf136d1a41f704ca8e4'
+            }
+        })
+    });
+});
